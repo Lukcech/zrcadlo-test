a358c5e40f9c05ec5c7f5e6d5dc8ae14fd4ee1fb
Lukcech <145280602+Lukcech@users.noreply.github.com>
2026-08-20 11:46:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import uuid
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct, VectorParams, Distance
 
-API_KEY = "AIzaSyAb8RN6LcRITU_9D9RMh8jYDkFLPLLgVantVqKGzDhnpmxrzZbw"
+API_KEY = st.secrets["API_KEY"]
 DB_PATH = "./qdrant_db"
 GRAPH_DB_PATH = "znalostni_graf.db"
 
